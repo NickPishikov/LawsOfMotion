f927e8c2b7ede096a667bb308fb5d8cf0c5c362a
Nick_Pishikov <NickPishikov2011@yandex.ru>
2020-03-10 21:34:01 +0300

    FirstCommit

---
+++ LawOfMotion/LawsOfMotion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawsOfMotion
+{
+  abstract class LawOfMotion
+    {
+        double Alf;//угол
+        double Velocity;//скорость
+        double Time;//время
+        double Acceleration;//ускорение
+        double Spath;//Путь
+    }
+}
